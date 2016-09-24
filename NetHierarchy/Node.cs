7f5d3a56e5801e512c1f2fa5b3f2ad77b1f0c7e9
Scottz0r <scott.clewell@gmail.com>
2016-09-24 11:14:07 -0500

    Initial Commit

---
+++ NetHierarchy/Node.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetHierarchy
+{
+    /// <summary>
+    /// Represents a point on a hierarchy and defines methods to manipulate hierarchies.
+    /// </summary>
+    /// <typeparam name="T">The type of data that will be sotred in the node.</typeparam>
+    public class Node<T>
+    {
+        /// <summary>
+        /// Get or set that data that belongs to the node.
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// Get or set an <see cref="ICollection{T}"/> of nodes that are a child to the node.
+        /// </summary>
+        public ICollection<Node<T>> Children { get; set; }
+
+        /// <summary>
+        /// Gets or sets the parent node of this node.
+        /// </summary>
+        public Node<T> Parent { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating if the node is a leaf-node. A leaf-node has no children.
+        /// </summary>
+        public bool IsLeaf
+        {
+            get { return Children.Count == 0; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating if the node is a root-node. A root-node is at the top of hierarchy and has no parents.
+        /// </summary>
+        public bool IsRoot
+        {
+            get { return Parent == null; }
+        }
+
+        #region Constructor
+
+        #region Without Children
+        public Node()
+        {
+            this.Children = new List<Node<T>>();
+        }
+
+        public Node(T Data) : this()
+        {
+            this.Data = Data;
+        }
+
+        public Node(T Data, Node<T> Parent) : this(Data)
+        {
+            this.Parent = Parent;
+
+            if (!Parent.Children.Contains(this))
+                Parent.AddChild(this);
+        }
+
+        public Node(T Data, T ParentData) : this(Data)
+        {
+            var parentNode = new Node<T>(ParentData);
+            parentNode.AddChild(this);
+        }
+
+        #endregion
+
+        #region With Children
+        public Node(ICollection<Node<T>> Children)
+        {
+            this.Children = Children;
+        }
+
+        public Node(T Data, ICollection<Node<T>> Children) : this(Children)
+        {
+            this.Data = Data;
+        }
+
+        public Node(T Data, ICollection<Node<T>> Children, Node<T> Parent) :this(Data, Children)
+        {
+            this.Parent = Parent;
+            if (!Parent.Children.Contains(this))
+                Parent.AddChild(this);
+        }
+
+        public Node(T Data, ICollection<Node<T>> Children, T Parent) : this(Data, Children, new Node<T>(Parent))
+        {
+            
+        }
+        #endregion
+
+        #endregion
+
+        /// <summary>
+        /// Add a child to this node and creates the parent/child relationship between the nodes.
+        /// </summary>
+        /// <param name="ChildNode">The child item to add.</param>
+        public void AddChild(Node<T> ChildNode)
+        {
+            if (ChildNode == null) throw new ArgumentNullException(nameof(ChildNode));
+
+            this.Children.Add(ChildNode);
+            ChildNode.Parent = this;
+        }
+
+        /// <summary>
+        /// Creates an <see cref="IEnumerable{T}"/> of all of the nodes underneath and including this node.
+        /// </summary>
+        public IEnumerable<Node<T>> GetDescendants()
+        {
+            yield return this;
+
+            foreach(var node in Children)
+            {
+                foreach(var childAll in node.GetDescendants())
+                {
+                    yield return childAll;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get all of the ancestors of the <see cref="Node{T}"/>. 
+        /// </summary>
+        public IEnumerable<Node<T>> GetAncestors()
+        {
+            if(this.Parent != null)
+            {
+                yield return this.Parent;
+
+                foreach (var ancestor in this.Parent.GetAncestors())
+                    yield return ancestor;
+            }
+        }
+
+        /// <summary>
+        /// Checks to see if the <see cref="Node{T}"/> is a child of the given <see cref="Node{T}"/>.
+        /// Checks are done with reference checks.
+        /// </summary>
+        /// <param name="Check">The <see cref="Node{T}"/> to check under.</param>
+        public bool IsDescendantOf(Node<T> Check)
+        {
+            if (Check == null) throw new ArgumentNullException(nameof(Check));
+
+            //No parent then it is not a descendant of anything.
+            if (this.Parent == null)
+                return false;
+
+            //If Check is immediate parent, then true.
+            if(object.ReferenceEquals(this.Parent, Check))
+            {
+                return true;
+            }
+            //Else begin walking up the hierarchy.
+            else
+            {
+                return this.Parent.IsDescendantOf(Check);
+            }
+        }
+
+        #region Linq Like Methods
+
+        /// <summary>
+        /// Filters the descendant nodes based on a predicate.
+        /// </summary>
+        public IEnumerable<Node<T>> DescendantsWhere(Func<Node<T>, bool> predicate)
+        {
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+            if (predicate(this))
+                yield return this;
+
+            foreach(var child in Children)
+            {
+                foreach(var result in child.DescendantsWhere(predicate))
+                {
+                    yield return result;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Object Overrides
+        public override string ToString()
+        {
+            return this.Data.ToString();
+        }
+        #endregion
+    }
+}
---
+++ NetHierarchyTests/Node_Tests.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetHierarchy;
+using System.Linq;
+
+namespace NetHierarchyTests
+{
+    [TestClass]
+    public class Node_Tests
+    {
+        [TestMethod]
+        public void Node_IsLeaf_NoChildren()
+        {
+            var node = new Node<int>();
+
+            var actual = node.IsLeaf;
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void Node_IsLeaf_WithChildren()
+        {
+            var node = new Node<int>();
+            node.Children.Add(new Node<int>(1));
+
+            var actual = node.IsLeaf;
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void Node_AddChild()
+        {
+            var node = new Node<int>(1);
+            var childNode = new Node<int>(2);
+
+            node.AddChild(childNode);
+
+            Assert.AreEqual(childNode, node.Children.ElementAt(0));
+            Assert.AreEqual(node, childNode.Parent);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Node_AddChild_ArgumentNull()
+        {
+            var node = new Node<int>(1);
+            Node<int> childNode = null;
+
+            node.AddChild(childNode);
+        }
+
+        [TestMethod]
+        public void Node_GetDescendants()
+        {
+            var root = new Node<string>("Root");
+            var child1 = new Node<string>("Child1");
+            var child2 = new Node<string>("Child2");
+            var child1Child = new Node<string>("Child1Child");
+
+            root.AddChild(child1);
+            root.AddChild(child2);
+            child1.AddChild(child1Child);
+
+            var actual = root.GetDescendants();
+            var actualList = actual.ToList();
+
+            Assert.AreEqual(4, actual.Count());
+            CollectionAssert.Contains(actualList, root);
+            CollectionAssert.Contains(actualList, child1);
+            CollectionAssert.Contains(actualList, child2);
+            CollectionAssert.Contains(actualList, child1Child);
+        }
+
+        [TestMethod]
+        public void Node_GetDescendants_Singe()
+        {
+            var root = new Node<string>("Root");
+
+            var actual = root.GetDescendants();
+            var actualList = actual.ToList();
+
+            Assert.AreEqual(1, actual.Count());
+            CollectionAssert.Contains(actualList, root);
+        }
+
+        [TestMethod]
+        public void Node_GetAncestors()
+        {
+            var root = new Node<string>("Root");
+            var grandparent = new Node<string>("Grandparent");
+            var parent = new Node<string>("Parent");
+            var parent2 = new Node<string>("Parent2");
+            var child = new Node<string>("Child");
+
+            root.AddChild(grandparent);
+            grandparent.AddChild(parent);
+            grandparent.AddChild(parent2);
+            parent.AddChild(child);
+
+            var actual = child.GetAncestors();
+            var actualList = actual.ToList();
+
+            Assert.AreEqual(3, actualList.Count);
+            CollectionAssert.Contains(actualList, root);
+            CollectionAssert.Contains(actualList, grandparent);
+            CollectionAssert.Contains(actualList, parent);
+        }
+
+        [TestMethod]
+        public void Node_GetAncestors_NoParent()
+        {
+            var root = new Node<string>("Root");
+
+            var actual = root.GetAncestors();
+
+            Assert.AreEqual(0, actual.Count());
+        }
+
+        [TestMethod]
+        public void Node_IsDescendantOf()
+        {
+            var root = new Node<string>("Root");
+            var grandparent = new Node<string>("Grandparent");
+            var parent = new Node<string>("Parent");
+            var child = new Node<string>("Child");
+
+            root.AddChild(grandparent);
+            grandparent.AddChild(parent);
+            parent.AddChild(child);
+
+            var actual = child.IsDescendantOf(root);
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void Node_IsDescendantOf_NotDescendant()
+        {
+            var root = new Node<string>("Root");
+            var grandparent = new Node<string>("Grandparent");
+            var parent = new Node<string>("Parent");
+            var parent2 = new Node<string>("Parent2");
+            var child = new Node<string>("Child");
+
+            root.AddChild(grandparent);
+            grandparent.AddChild(parent);
+            grandparent.AddChild(parent2);
+            parent.AddChild(child);
+
+            var actual = child.IsDescendantOf(parent2);
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void Node_IsDescendantOf_Immediate()
+        {
+            var grandparent = new Node<string>("Grandparent");
+            var parent = new Node<string>("Parent");
+
+            grandparent.AddChild(parent);
+
+            var actual = parent.IsDescendantOf(grandparent);
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void Node_IsDescendantOf_NoParent()
+        {
+            var node = new Node<string>("LittleOrphanBoy");
+            var other = new Node<string>("OtherNode");
+
+            var actual = node.IsDescendantOf(other);
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Node_IsDescendantOf_NullException()
+        {
+            var node = new Node<string>("Cats");
+
+            var actual = node.IsDescendantOf(null);
+        }
+
+        [TestMethod]
+        public void Node_DescendantsWhere()
+        {
+            var root = new Node<int>(1);
+            var grandparent = new Node<int>(2);
+            var parent = new Node<int>(3);
+            var parent2 = new Node<int>(4);
+            var child = new Node<int>(5);
+
+            root.AddChild(grandparent);
+            grandparent.AddChild(parent);
+            grandparent.AddChild(parent2);
+            parent.AddChild(child);
+
+            var actual = root.DescendantsWhere(x => x.Data > 2);
+            var actualList = actual.ToList();
+
+            Assert.AreEqual(3, actualList.Count);
+            CollectionAssert.Contains(actualList, parent);
+            CollectionAssert.Contains(actualList, parent2);
+            CollectionAssert.Contains(actualList, child);
+        }
+
+        [TestMethod]
+        public void Node_DescendantsWhere_NoMatch()
+        {
+            var root = new Node<int>(1);
+            var grandparent = new Node<int>(2);
+            var parent = new Node<int>(3);
+            var parent2 = new Node<int>(4);
+            var child = new Node<int>(5);
+
+            root.AddChild(grandparent);
+            grandparent.AddChild(parent);
+            grandparent.AddChild(parent2);
+            parent.AddChild(child);
+
+            var actual = root.DescendantsWhere(x => x.Data == 1337);
+            var actualList = actual.ToList();
+
+            Assert.AreEqual(0, actualList.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Node_DescendantsWhere_ArgumentNull()
+        {
+            var root = new Node<int>(1);
+
+            var actual = root.DescendantsWhere(null);
+            var actualList = actual.ToList();
+        }
+    }
+}
